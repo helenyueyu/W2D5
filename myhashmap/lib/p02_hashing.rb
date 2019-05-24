36183ee16eaf8f351b9c6b4554485b2b30a505d0
helen_yu <helenyueyu@gmail.com>
2019-05-24 15:16:55 -0700

    Complete hash function

--- myhashmap/lib/p02_hashing.rb
+++ myhashmap/lib/p02_hashing.rb
@@ -4,11 +4,13 @@ end
 
 class Array
   def hash
+    self.each_with_index.map{|x,idx| [x, idx]}.map{|x| x[0]*x[1]}.sum.hash 
   end
 end
 
 class String
   def hash
+    self.chars.each_with_index.map{|x, idx| [x, idx]}.map{|x| x[0].ord*x[1]}.sum.hash 
   end
 end
 
@@ -16,6 +18,19 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    arr = [] 
+    self.keys.each do |key| 
+      arr << [key, self[key]]
+    end
+    arr = arr.sort_by{|x| x[0]}
+
+    arr = arr.map{|x| [x[0].hash, x[1].hash]}
+
+    hash_val = 0 
+    arr.each do |subarr|
+      hash_val += subarr.hash 
+    end 
+    
+    return hash_val 
   end
 end
