ba99a8dba072eb10de5ae1d4cfd901efc1b93d6e
helen_yu <helenyueyu@gmail.com>
2019-05-24 15:29:40 -0700

    Hash set

--- myhashmap/lib/p03_hash_set.rb
+++ myhashmap/lib/p03_hash_set.rb
@@ -7,12 +7,20 @@ class HashSet
   end
 
   def insert(key)
+    @store[key.hash % num_buckets] << key
+    @count += 1 
+    resize! if @count >= num_buckets
   end
 
   def include?(key)
+    @store[key.hash % num_buckets].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      @store[key.hash % num_buckets].delete(key)
+      @count -= 1
+    end
   end
 
   private
@@ -26,5 +34,11 @@ class HashSet
   end
 
   def resize!
+      vals = @store.flatten
+      @store = Array.new(num_buckets*2) { Array.new }
+      @count = 0
+      vals.each do |val|
+        insert(val)
+      end
   end
 end
