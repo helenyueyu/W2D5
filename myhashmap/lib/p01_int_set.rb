e1e38a77001b47849a8881edeea2872c8edbaf01
helen_yu <helenyueyu@gmail.com>
2019-05-24 14:38:51 -0700

    Finish int set, resizing int set

---
+++ myhashmap/lib/p01_int_set.rb
@@ -0,0 +1,108 @@
+class MaxIntSet
+
+  attr_reader :store
+  def initialize(max)
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    @store[num] = true if is_valid?(num)
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    return true if @store[num] == true
+  end
+
+  private
+
+  def is_valid?(num)
+    if num > @store.length || num < 0
+      raise "Out of bounds"
+    end
+    true
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    @store[num % @store.length].push(num)
+  end
+
+  def remove(num)
+    @store[num % @store.length].delete(num)
+  end
+
+  def include?(num)
+    @store[num % @store.length].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    if !include?(num)
+      @store[num % num_buckets].push(num)
+      @count += 1
+      resize! if @count >= num_buckets
+    end
+  end
+
+  def remove(num)
+    if include?(num)
+      @store[num % num_buckets].delete(num)
+      @count -= 1
+    end
+  end
+
+  def include?(num)
+    @store[num % num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    vals = @store.flatten
+    @store = Array.new(num_buckets*2) { Array.new }
+    @count = 0
+    vals.each do |val|
+      insert(val)
+    end
+  end
+
+end
