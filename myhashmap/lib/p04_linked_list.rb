97e81bd9b8be00b181309c7625f142072acf1bde
helen_yu <helenyueyu@gmail.com>
2019-05-24 17:00:13 -0700

    Linked list

--- myhashmap/lib/p04_linked_list.rb
+++ myhashmap/lib/p04_linked_list.rb
@@ -20,7 +20,12 @@ class Node
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Node.new 
+    @tail = Node.new
+    @head.next = @tail 
+    @tail.prev = @head 
   end
 
   def [](i)
@@ -29,30 +34,57 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail 
   end
 
   def get(key)
+    each{|node| return node.val if node.key == key}
+    nil
   end
 
   def include?(key)
+    return false if get(key).nil?
+    true
   end
 
   def append(key, val)
+
+    node = Node.new(key, val)
+
+    @tail.prev.next = node 
+    node.prev = @tail.prev
+    
+    @tail.prev = node
+    node.next = @tail 
   end
 
   def update(key, val)
+    each{|node| node.val = val if node.key == key}
   end
 
   def remove(key)
+    curr_node = nil
+    
+    each{|node| curr_node = node if node.key == key}
+
+    curr_node.prev.next = curr_node.next 
+    curr_node.next.prev = curr_node.prev
   end
 
-  def each
+  def each(&prc)
+    curr_node = @head.next 
+    while curr_node != @tail 
+      prc.call(curr_node)
+      curr_node = curr_node.next 
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
